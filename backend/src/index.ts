9c36b0f0b31ade8d01dcdc65db83216ab2823ecf
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-06-29 04:45:19 +0530

    feat: add middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { prisma } from "@/libs/db";
+import { log } from "@/libs/logger";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT = process.env.PORT || 5000;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+	.use(cors())
+	.use(swagger())
+	.use(log)
+	.decorate("db", prisma);
+
+app.get("/", () => "Hello from Elysia");
+
+app.listen(PORT, () => {
+	console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`);
+});
