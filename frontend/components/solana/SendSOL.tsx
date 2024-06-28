a703df8e6a21375cbb27fd0910b6e87e83d6f903
Ankit-AP-Paul <ankitpaul894@gmail.com>
2024-06-28 21:19:10 +0530

    added solana wallet payment

---
+++ frontend/components/solana/SendSOL.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { SystemProgram, Transaction, PublicKey } from "@solana/web3.js";
+import React, { useCallback } from "react";
+import { Button } from "../ui/button";
+
+interface Props {
+  lamps: number;
+  toKey: string;
+}
+export default function SendSOL({ lamps, toKey }: Props) {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const onClick = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+    const lamports = lamps;
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: new PublicKey(toKey),
+        lamports,
+      })
+    );
+
+    const {
+      context: { slot: minContextSlot },
+      value: { blockhash, lastValidBlockHeight },
+    } = await connection.getLatestBlockhashAndContext();
+
+    const signature = await sendTransaction(transaction, connection, {
+      minContextSlot,
+    });
+
+    await connection.confirmTransaction({
+      blockhash,
+      lastValidBlockHeight,
+      signature,
+    });
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <Button
+      className="generalBorder offsetEffect  bg-green-300 text-black hover:text-white"
+      onClick={onClick}
+      disabled={!publicKey}>
+      Send SOL
+    </Button>
+  );
+}
