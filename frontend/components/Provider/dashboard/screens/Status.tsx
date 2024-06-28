51bf01f0387160c674812da71aab025627a60e7d
diptanshumahish <diptanshumahish2016@gmail.com>
2024-06-29 01:17:23 +0530

    rent dashboard done

--- frontend/components/Provider/dashboard/screens/Status.tsx
+++ frontend/components/Provider/dashboard/screens/Status.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Status() {
     return (
-        <div>Status</div>
+        <div className='py-2'>
+            <h2 className='text-2xl font-semibold'>Daemon status</h2>
+            <div className='bg-green-200 generalBorder offsetEffect'>
+                RUNNING
+            </div>
+            <span className='my-2 text-xs'>last updated 20 secs ago</span>
+        </div>
     )
 }
---
+++ frontend/components/Rent/dashboard/RentTabs.tsx
@@ -0,0 +1,39 @@
+import ChipsButton from '@/components/common/ChipsButton'
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+
+interface Props {
+    onChange: (tab: number) => void;
+    activeIdx: number;
+}
+export default function RentTabs({ onChange, activeIdx }: Props) {
+    const [activeIndex, setActiveIndex] = useState(activeIdx);
+    return (
+        <div className='flex items-center gap-4 py-2'>
+            <ChipsButton content='Deployments' onClick={() => {
+                onChange(0);
+                setActiveIndex(0)
+            }} active={activeIndex === 0 ? true : false} />
+            <ChipsButton content='Cost Calculator' onClick={() => {
+                onChange(1)
+                setActiveIndex(1)
+            }} active={activeIndex === 1 ? true : false} />
+            <ChipsButton content='Transactions' onClick={() => {
+                onChange(2)
+                setActiveIndex(2)
+            }} active={activeIndex === 2 ? true : false} />
+            {/* <ChipsButton content='Wallet' onClick={() => {
+                onChange(3)
+                setActiveIndex(3)
+            }} active={activeIndex === 3 ? true : false} />
+            <ChipsButton content='Earnings' onClick={() => {
+                onChange(4)
+                setActiveIndex(4)
+            }} active={activeIndex === 4 ? true : false} />
+            <ChipsButton content='Transactions' onClick={() => {
+                onChange(5)
+                setActiveIndex(5)
+            }} active={activeIndex === 5 ? true : false} /> */}
+        </div>
+    )
+}
