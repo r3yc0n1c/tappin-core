9ba34b436739038d90670e8027571b78268fcc12
diptanshumahish <diptanshumahish2016@gmail.com>
2024-06-28 23:03:40 +0530

    not found page added

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='h-screen w-screen'>
+            <div className='w-[30%] offsetEffect generalBorder bg-red-300 text-black'>
+                <h2 className='font-bold text-2xl'>Page not found</h2>
+            </div>
+        </div>
+    )
+}
