3e1470c72544a2f5cfc23edcab269023698b28fb
diptanshumahish <diptanshumahish2016@gmail.com>
2024-06-29 18:26:46 +0530

    fixed login + register

---
+++ frontend/api-management/models/user.ts
@@ -0,0 +1,11 @@
+export class Login {
+    declare email: string;
+    declare password: string;
+}
+
+export class Register {
+    declare email: string;
+    declare password: string;
+    declare name: string;
+    declare address: string;
+}
