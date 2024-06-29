cdd77c8f8b16cc39f3408401fec3e06df8cc9366
diptanshumahish <diptanshumahish2016@gmail.com>
2024-06-30 00:48:34 +0530

    search page added✅

---
+++ frontend/app/(root)/search/page.tsx
@@ -0,0 +1,10 @@
+import SearchWrapper from '@/components/search/SearchWrapper'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='py-[3%]'>
+            <SearchWrapper />
+        </div>
+    )
+}
