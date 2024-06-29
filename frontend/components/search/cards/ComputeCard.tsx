acbdadb9a4ae2a10e210dcbd68ddb68e9f31f37b
ABHIRUP <abhirupsaha02@gmail.com>
2024-06-30 02:02:37 +0530

    api call for available machines

--- frontend/components/search/cards/ComputeCard.tsx
+++ frontend/components/search/cards/ComputeCard.tsx
@@ -12,23 +12,23 @@ export default function ComputeCard({ props }: Props) {
             <div className='grid grid-cols-2 gap-2'>
                 <div className='flex flex-col '>
                     <span className='text-xs text-gray-600'>Device name</span>
-                    <h1 className='text-xl font-medium'>{props.name}</h1>
+                    <h1 className='text-xl font-medium'>{props.title}</h1>
                 </div>
                 <div className='flex flex-col'>
                     <span className='text-xs text-gray-600'>Available CPU</span>
                     <h1 className='text-xl font-medium'>{props.cpu}</h1>
                 </div>
                 <div className='flex flex-col'>
-                    <span className='text-xs text-gray-600'>Available GPU</span>
-                    <h1 className='text-xl font-medium'>{props.gpu}</h1>
+                    <span className='text-xs text-gray-600'>Available RAM</span>
+                    <h1 className='text-xl font-medium'>{props.ram}GB</h1>
                 </div>
                 <div className='flex flex-col'>
                     <span className='text-xs text-gray-600'>Available space</span>
-                    <h1 className='text-xl font-medium'>{props.space}</h1>
+                    <h1 className='text-xl font-medium'>{props.storage}GB</h1>
                 </div>
                 <div className='flex flex-col'>
                     <span className='text-xs text-gray-600'>Instances</span>
-                    <h1 className='text-xl font-medium'>{props.activity}</h1>
+                    <h1 className='text-xl font-medium'>{props?.activity}</h1>
                 </div>
             </div>
             <div className='pt-2'>
--- frontend/constants/images/models/specscard.model.ts
+++ frontend/constants/images/models/specscard.model.ts
@@ -1,8 +1,9 @@
 export class SpecsCard {
-    declare id: string; //j82jjdjjjjHJHgshjkjj89981288j7shoxweeejua
-    declare name: string; //tappin.dip798
-    declare gpu: string; // 1/16GB
-    declare cpu: string; // 2/64GB
-    declare space: string; // 0.06/20TB
-    declare activity: number; // 3 (active leases)
+    declare id: string; 
+    declare title: string;
+    declare cpu: string;
+    declare ram: string;
+    declare storage: string;
+    declare address: string;
+    declare activity?: number;
 }
