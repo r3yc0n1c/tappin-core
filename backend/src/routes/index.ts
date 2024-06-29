3e095fa44f6ef52ffe842e84baf8e4ced05f3fd6
ABHIRUP <abhirupsaha02@gmail.com>
2024-06-29 21:26:37 +0530

    generate payment url route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Elysia } from "elysia";
 import authRoutes from "@/routes/auth";
 import machineRoutes from "@/routes/machine";
+import paymentRoutes from "@/routes/payment";
 
 const routes = new Elysia();
 
 routes
 	.get("/", () => "Hello from Elysia!")
 	.group("/auth", (routes) => routes.use(authRoutes))
-	.group("/machine", (routes) => routes.use(machineRoutes));
+	.group("/machine", (routes) => routes.use(machineRoutes))
+	.group("/payment", (routes)  => routes.use(paymentRoutes));
 
 export default routes;
